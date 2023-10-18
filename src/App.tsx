69c9c3974bb008a811a4c3b834b3808dcf7f3ef3
girimelorra <giriraj.nagar@melorra.com>
2023-10-18 14:17:38 +0530

    added other repos

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import {Button} from '@melorra/component';
+import {ButtonTypeTokens} from '@melorra/schema';
 import React from 'react';
 import {SafeAreaView, StatusBar, Text, View} from 'react-native';
 
@@ -12,6 +14,11 @@ const App = () => {
           margin: 12,
         }}>
         <Text style={{fontSize: 24}}>Hello World</Text>
+
+        <Button.Component
+          type={ButtonTypeTokens.LargeFilled}
+          label="This is button"
+        />
       </View>
     </SafeAreaView>
   );
